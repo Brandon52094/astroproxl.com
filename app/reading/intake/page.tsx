69559a98f9481698a4c3fa06a93536920b9bf2fa
Brandon Johnson <contactbrandonjohnson@gmail.com>
2026-06-27 04:54:46 -0700

    Add missing lib/signVoice.ts shared module

--- app/reading/intake/page.tsx
+++ app/reading/intake/page.tsx
@@ -1,5 +1,6 @@
 import ReadingIntakeScreen from "@/app/components/ReadingIntakeScreen";
 
+export const dynamic = "force-dynamic";
 export default function Page() {
   return <ReadingIntakeScreen />;
 }
