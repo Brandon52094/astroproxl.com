03dcd1c9031f7e50ebbf3e55301cd4a4d51a61c9
Brandon Johnson <contactbrandonjohnson@gmail.com>
2026-08-07 11:06:12 -0700

    Update

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,4 +1,4 @@
-import type { MetadataRoute } from "next";
+`import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
@@ -25,4 +25,4 @@ export default function manifest(): MetadataRoute.Manifest {
       },
     ],
   };
-}
+}`
