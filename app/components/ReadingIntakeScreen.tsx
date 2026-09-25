79a92ddcf62b27784168b3f75f87a5f5e91d0356
Brandon Johnson <contactbrandonjohnson@gmail.com>
2026-09-24 21:23:16 -0700

    Update wording in reading intake page

--- app/components/ReadingIntakeScreen.tsx
+++ app/components/ReadingIntakeScreen.tsx
@@ -1258,7 +1258,6 @@ export default function ReadingIntakeScreen({
                 </div>
               </div>
             </div>
-            </div>
           </section>
 
           {/* ── Dynamic reading header ──
